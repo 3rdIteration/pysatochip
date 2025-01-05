59ce7955aebb46a45d0a2e74e9d0102f82053609
3rd Iteration <steve@cryptoguide.tips>
2025-01-06 07:06:37 +1000

    Update satochip_cli.py

--- satochip_cli.py
+++ satochip_cli.py
@@ -1339,7 +1339,7 @@ def seedkeeper_export_secret(sid, pubkey_id, export_dict):
 
                     if login_length > 0:
                         login_text = binascii.unhexlify(secret_dict['secret'])[
-                                     password_length + 1: password_length + login_length + 2].decode()
+                                     password_length + 2: password_length + login_length + 2].decode()
 
                         secret_string += "\nLogin:" + "\"" + login_text + "\""
 
